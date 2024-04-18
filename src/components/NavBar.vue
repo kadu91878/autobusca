06a24f2bb18a36f6451b60ac732823ad73e8b840
kadu91878 <kadu_91878@hotmail.com>
2024-04-18 18:15:58 -0300

    feat: Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
+import { IosSearch } from "@vicons/ionicons4";
 
+//Refs
+const busca = ref("");
 </script>
 
 <template>
@@ -10,19 +14,31 @@ import { RouterLink, RouterView } from 'vue-router'
         <img src="../assets/logo.png" alt="" />
       </div>
       <div class="_title">
-        <h1>Autobusca</h1>
+        <h1>AutoBusca</h1>
       </div>
     </div>
     <div class="left-side">
-        <div class="links">
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/">Quero Comprar</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-          <RouterLink to="/contact">Contact</RouterLink>
-          <RouterLink to="/login">Login</RouterLink>
-          <RouterLink to="/register">Register</RouterLink>
-
-        </div>
+      <div class="links">
+        <RouterLink class="link" to="/">Quero Comprar</RouterLink>
+        <RouterLink class="link"  to="/">Home</RouterLink>
+        <RouterLink class="link" to="/about">About</RouterLink>
+        <RouterLink class="link" to="/contact">Contact</RouterLink>
+        <RouterLink class="link" to="/login">Login</RouterLink>
+        <RouterLink class="link" to="/register">Register</RouterLink>
+      </div>
+      <div class="search-bar">
+        <n-input
+          class="input"
+          v-model:value="busca"
+          type="text"
+          placeholder="Pesquise aqui..."
+          round
+        >
+          <template #prefix>
+            <n-icon size="20" :component="IosSearch" />
+          </template>
+        </n-input>
+      </div>
     </div>
   </div>
 </template>
@@ -31,7 +47,7 @@ import { RouterLink, RouterView } from 'vue-router'
 ._container {
   width: 100%;
   display: flex;
-flex-direction: row;
+  flex-direction: row;
   align-items: center;
   background-color: #f6bd17;
   img {
@@ -39,26 +55,43 @@ flex-direction: row;
   }
 }
 
-.right-side{
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    flex-direction: row;
-    margin-left: 10vh;
+.right-side {
+  width: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: row;
+  margin-left: 10vh;
 }
 
-.left-side{
-    width: 100%;
+.left-side {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-right: 10vh;
+  gap: 20px;
+  .links {
     display: flex;
+    flex-direction: row;
     align-items: center;
-    justify-content: flex-end;
-    margin-right: 10vh;
-    .links{
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 20px;
-    }
+    gap: 20px;
+  }
+}
+
+.link{
+  text-decoration: none;
+  font-family: 'Paytone one';
+  font-weight: 900;
+  color: black;
+  font-size: 15px;
+}
+
+.link:hover{
+  color: white;
+}
+
+._title{
+    font-family: 'Paytone one';
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,23 @@ import naive from "naive-ui";
 
 import App from './App.vue'
 import router from './router'
+import WebFontLoader from 'webfontloader';
 
 const app = createApp(App)
+// Carrega as fontes usando o createWebFontLoader
+WebFontLoader.load({
+    google: {
+        families: [
+            'Assistant:700',
+            'Cormorant Infant:300,400,500,600,700',
+            'Encode Sans:100..900',
+            'Hind:300,500,700',
+            'Paytone One:regular&display=swap',
+        ]
+    }
+});
+
+
 
 app.use(createPinia())
 app.use(router)
